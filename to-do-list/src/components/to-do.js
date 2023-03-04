2b2e29ad6b97bed0702d7d45ece08094c94e95bc
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-03-04 13:12:24 +0530

    update project

--- to-do-list/src/components/to-do.js
+++ to-do-list/src/components/to-do.js
@@ -3,12 +3,14 @@ import '../App.css'
 export const ToDo = () => {
     return (
         <>
-            <input placeholder="Enter Task" type="text"/>
-            <li>
-                Buy milk 
-                <i class="fa-solid fa-square-check"></i>
-                <i class="fa-solid fa-trash"></i>
-            </li>
+            <form className='form'>
+                <input className='input' placeholder="Enter Task" type="text"/>
+                <li>
+                    Buy milk 
+                    <i className="fa-solid fa-square-check"></i>
+                    <i className="fa-solid fa-trash"></i>
+                </li>
+            </form>
         </>
     )
 }
